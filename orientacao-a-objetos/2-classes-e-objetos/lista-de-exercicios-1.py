a935b9702dfb0dcff6c797fdcf85bbc645ae4d57
Clarisse Simoes <clasimoesr@gmail.com>
2023-08-05 18:32:03 -0300

    Adicionando primeira ista de exercicios

---
+++ orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
@@ -0,0 +1,13 @@
+# Crie uma classe que modele o objeto "carro".
+# Um carro tem os seguintes atributos: ligado, cor, modelo, velocidade.
+# Um carro tem os seguintes comportamentos: liga, desliga, acelera, desacelera.
+
+
+
+# Crie uma instância da classe carro.
+
+
+# Faça o carro "andar" utilizando os métodos da sua classe.
+
+
+# Faça o carro "parar" utilizando os métodos da sua classe.
